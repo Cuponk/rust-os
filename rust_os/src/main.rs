20ece5c2449a2fa0f703c53a8866e2f4335f7a36
Cuponk <cuponk64@gmail.com>
2022-09-15 05:39:00 -0700

    add x86_64-blog_os.json

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -1,5 +1,8 @@
-#![no_std]
-#![no_main]
+// src/main.rs
+
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
@@ -8,7 +11,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
     loop {}
 }

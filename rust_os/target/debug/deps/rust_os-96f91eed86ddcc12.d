58c76f83c472520eac5e40aedf87a1baf4739774
Cuponk <cuponk64@gmail.com>
2022-09-14 19:33:00 -0700

    build cargo

---
+++ rust_os/target/debug/deps/rust_os-96f91eed86ddcc12.d
@@ -0,0 +1,5 @@
+/home/justin/repos/rust-os/rust_os/target/debug/deps/rust_os-96f91eed86ddcc12: src/main.rs
+
+/home/justin/repos/rust-os/rust_os/target/debug/deps/rust_os-96f91eed86ddcc12.d: src/main.rs
+
+src/main.rs:
---
+++ rust_os/target/debug/rust_os.d
@@ -0,0 +1,1 @@
+/home/justin/repos/rust-os/rust_os/target/debug/rust_os: /home/justin/repos/rust-os/rust_os/src/main.rs
